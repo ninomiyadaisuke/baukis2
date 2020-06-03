3432d2a9547418bfcee49b8aeb0e5cda39c03dd4
”ninomiyadaisuke” <daisukeninomiya0375@gmail.com>
2020-06-03 15:28:17 +0900

    Section 5-1-3

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+# Rails.application.configure do
+#   #yarnのエラーメッセージに対する対処法
+#  config.webpacker.check_yarn_integrity = false
+# end
