f552dc3e8be393c6761888237911de5eeb1532a9
”ninomiyadaisuke” <daisukeninomiya0375@gmail.com>
2020-06-08 11:45:59 +0900

    Section 6-4-1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
-rescue_from StandardError, with: :rescue500
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
+  rescue_from StandardError, with: :rescue500
+  rescue_from Forbidden, with: :rescue403
+  rescue_from IpAddressRejected, with: :rescue403
 
   private def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
@@ -10,6 +15,14 @@ rescue_from StandardError, with: :rescue500
       "customer"
     end
   end
+
+  private def rescue403(e)
+    @exception = e
+    render "errors/forbidden", status: 403
+  end
+
+
+
 
   private def rescue500(e)
     render "errors/internal_server_error", status: 500
