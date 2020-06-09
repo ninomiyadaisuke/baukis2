15129200fe09ea9e30fadff48f45838ff05f018e
”ninomiyadaisuke” <daisukeninomiya0375@gmail.com>
2020-06-09 10:41:12 +0900

    Section 7-3-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :staff do
     root "top#index"
+    get "login" => "sessions#new", as: :login
+    post "session" => "sessions#create", as: :session
+    delete "session" => "sessions#destroy"
   end
 
   namespace :admin do
@@ -9,5 +12,5 @@ Rails.application.routes.draw do
 
   namespace :customer do
     root "top#index"
-  end  
+  end
 end
